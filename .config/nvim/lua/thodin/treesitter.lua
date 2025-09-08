96cf5c2f5006fbf1efd104a2b160ac1d4b0be9a2
Max Meindl <max_meindl@gmx.de>
2025-09-08 20:42:46 +0200

    Add typescript language server, linter and prettier

--- .config/nvim/lua/thodin/treesitter.lua
+++ .config/nvim/lua/thodin/treesitter.lua
@@ -1,6 +1,6 @@
 -- treesitter
 require'nvim-treesitter.configs'.setup {
-	ensure_installed = { 'lua', 'vim', 'bash', 'markdown', 'json', 'rust' },
+	ensure_installed = { 'lua', 'vim', 'bash', 'markdown', 'json', 'rust', 'typescript', 'tsx', 'javascript', 'html', 'css' },
 	highlight = {
 		enabled = true,
 		additional_vim_regex_highlighting = false,
